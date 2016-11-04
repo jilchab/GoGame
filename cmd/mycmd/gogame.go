e3a2317080bd479c925d498fa4b6f89f821d9698
jilchab <jilchabaro@gmail.com>
2016-11-04 15:11:31 +0100

    commit

--- cmd/mycmd/gogame.go
+++ cmd/mycmd/gogame.go
@@ -1,18 +1,39 @@
 package main
 
+import (
+	. "github.com/jilchab/GoGame/mypkg/game"
+	"fmt"
+)
 
-import "fmt"
-import. "github.com/jilchab/GoGame/mypkg/game"
 
 
 
 func main () {
 
-	Player := Perso{"Hero",1,100,Weapon{"sword",20}}
-	//Gobgob := Perso{"Gobgob",1,30,Weapon{"pickaxe",10}}
+	pickaxe := Weapon{Name:"pickaxe",Damage:10}
+	sword := Weapon{Name:"sword",Damage:20}
+
+	Player := Perso{Name:"Hero",Lvl:1,Hp:100,Wpn:sword}
+	var Gobgobs []Perso
 
 
-	fmt.Println(Player)
+
+	Gobgobs = append(Gobgobs,
+		Perso{Name:"Jean",Lvl:1,Hp:50,Wpn:pickaxe},
+		Perso{Name:"Claude",Lvl:1,Hp:30,Wpn:pickaxe},
+		Perso{Name:"Gobgob",Lvl:1,Hp:10,Wpn:pickaxe},
+		Perso{Name:"Paul",Lvl:1,Hp:5,Wpn:pickaxe})
+
+
+
+	for i :=range Gobgobs {
+		Player.Attack(&Gobgobs[i])
+	}
+	CleanDeads(Gobgobs[:])
+
+	for _,g:=range Gobgobs {
+		fmt.Println(g)
+	}
 
 
 }
--- mypkg/game/Perso.go
+++ mypkg/game/Perso.go
@@ -1,6 +1,7 @@
 package game
 
 import "fmt"
+//import. "unsafe"
 
 type Perso struct {
 
@@ -8,19 +9,43 @@ type Perso struct {
 	Lvl, Hp int
 	Wpn Weapon
 }
-func (p *Perso) Attack(enemy *Perso) {
-	if p.Hp == 0 {return}
+
+func (p *Perso) Attack(enemy *Perso) (b bool){
+	if p.Hp == 0 {b = false}
 	if enemy.Hp > 0 {
 		enemy.Hp -= p.Wpn.Damage
 		fmt.Printf("%s attacks %s with his %s\n",p.Name,enemy.Name,p.Wpn.Name)
 		if(enemy.Hp < 0) {
 			enemy.Hp = 0
 			fmt.Printf("%s is dead\n", enemy.Name)
+			//i := (Sizeof(enemy)-Sizeof(array))/Sizeof(array[0])
+			//array = append(array[:i],array[i+1]...)
+			b = true
+
 		} else {
 			fmt.Printf("%s has now %d hp\n",enemy.Name, enemy.Hp)
 		}
-	} else {fmt.Println(enemy.Name,"is already dead!")}
+	} else {
+		fmt.Println(enemy.Name,"is already dead!")
+		b =  true
+	}
 
+	b = false
+	return
 
+}
 
+func CleanDeads(a []Perso) {
+	var aCopy []Perso;
+	copy(aCopy, a)
+
+	for i:=range aCopy {
+		fmt.Println(aCopy[i])
+	}
+
+	for i :=range aCopy {
+		if(aCopy[i].Hp == 0) {
+			a = append(a[:i],a[i+1:]...)
+		}
+	}
 }
